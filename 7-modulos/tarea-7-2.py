a24794a81d74facf09fdb61e9140dcce8dd2a84e
BryanFTC <fticonac@outlook.es>
2023-04-21 13:02:49 -0500

    Update tarea-7-2.py

--- 7-modulos/tarea-7-2.py
+++ 7-modulos/tarea-7-2.py
@@ -7,6 +7,23 @@
 
 
 import time
-hora = time.localtime()
-def hora_de_ir_a_casa(hora):
-    if hora > 7
+
+# Formateamos el tiempo para que solo se vean las horas y minutos
+
+hora = time.strftime('%H') 
+minutos = time.strftime('%M') 
+
+# Creamos una funcion que nos diga si es hora de ir a casa
+
+def hora_de_ir_a_casa(hora, minutos):
+    
+    hora_restantes = 18 - int(hora)
+    minutos_restantes = 59 - int(minutos)
+    if int(hora) >= 19:
+        print ("Es hora de irse a casa.")
+    else:
+        print (f"Quedan {hora_restantes} horas y {minutos_restantes} minutos para irnos a casa.")  # noqa: E501
+
+# Ejemplo
+
+print(hora_de_ir_a_casa(13, 30))
